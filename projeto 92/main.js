e7fa4866c6cabbcaa81afb9651819282c552f8b6
rafad46 <130005055+rafad46@users.noreply.github.com>
2023-07-26 18:55:19 -0300

    Add files via upload

---
+++ projeto 92/main.js
@@ -0,0 +1,22 @@
+const { fabric } = require("./fabric");
+
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL('AniversarioImagem.png', function(Img) {
+blockImageObject = img
+blockImageObject.scaleToWidth(700);
+blockImageObject.scaleToHeight(510);
+blockImageObject.set({
+    top:0,
+    left:0
+})
+canvas.add(blockImageObject)
+});
+}
+
+function playSound(){
+x.play
+}
